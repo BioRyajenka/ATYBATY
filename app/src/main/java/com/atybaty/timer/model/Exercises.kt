f840a432ae798139e435d49e39d2790a3e2c2aee
Igor <ism@veeroute.com>
2019-08-05 15:59:10 +0300

    Reorder classes

--- app/src/main/java/com/atybaty/timer/model/Exercises.kt
+++ app/src/main/java/com/atybaty/timer/model/Exercises.kt
@@ -5,6 +5,22 @@ import java.time.Duration
 
 sealed class Exercise
 
+data class Work(val name: String, val options: WorkOptions) : Exercise()
+
+abstract class Relaxation(val nameStringId: Int) : Exercise() {
+    abstract val duration: Duration
+}
+
+
+
+data class RunUp(override val duration: Duration) : Relaxation(R.string.exercises_run_up)
+
+data class RestBetweenSets(override val duration: Duration) : Relaxation(R.string.exercises_rest_between_sets)
+
+data class CalmDown(override val duration: Duration) : Relaxation(R.string.exercises_calm_down)
+
+
+
 sealed class WorkOptions
 
 data class SimpleWorkOptions(val duration: Duration) : WorkOptions()
@@ -12,15 +28,3 @@ data class SimpleWorkOptions(val duration: Duration) : WorkOptions()
 data class WorkWithAccelerationOptions(val accelerationDuration: Duration) : WorkOptions()
 
 data class WorkWithIntervalsOptions(val interval: Duration, val rattle: Duration)
-
-data class Work(val name: String, val options: WorkOptions) : Exercise()
-
-abstract class Relaxation(val nameStringId: Int) : Exercise() {
-    abstract val duration: Duration
-}
-
-data class RunUp(override val duration: Duration) : Relaxation(R.string.exercises_run_up)
-
-data class RestBetweenSets(override val duration: Duration) : Relaxation(R.string.exercises_rest_between_sets)
-
-data class CalmDown(override val duration: Duration) : Relaxation(R.string.exercises_calm_down)
