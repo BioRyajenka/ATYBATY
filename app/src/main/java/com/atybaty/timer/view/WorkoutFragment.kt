64bb2f4aebef83c5b070d84e660c003f6339c8ec
LevOspennikov <ospennikovlev@gmail.com>
2019-08-08 14:05:22 +0300

    skeleton of the screen

--- app/src/main/java/com/atybaty/timer/view/WorkoutFragment.kt
+++ app/src/main/java/com/atybaty/timer/view/WorkoutFragment.kt
@@ -9,19 +9,51 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.atybaty.timer.R
 import com.atybaty.timer.contract.WorkoutContract
 import com.atybaty.timer.contract.WorkoutListContract
+import com.atybaty.timer.model.ExerciseGroup
+import com.atybaty.timer.model.Workout
 import com.atybaty.timer.presenter.WorkoutListPresenter
 import com.atybaty.timer.presenter.WorkoutPresenter
+import com.atybaty.timer.view.adapters.ExerciseGroupAdapter
 import com.atybaty.timer.view.adapters.WorkoutAdapter
 import kotlinx.android.synthetic.main.activity_workout_list.*
+import kotlinx.android.synthetic.main.fragment_workout.*
 
-class WorkoutFragment : Fragment() {
+class WorkoutFragment : Fragment(), WorkoutContract.View {
+    private lateinit var workout: Workout
+
     private lateinit var presenter: WorkoutContract.Presenter
-    private lateinit var workoutAdapter: WorkoutAdapter
+    private lateinit var workoutAdapter: ExerciseGroupAdapter
+
+    fun setWorkout(workout: Workout) {
+        this.workout = workout
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        presenter = WorkoutPresenter(this, context!!)
+        workoutAdapter = ExerciseGroupAdapter(presenter)
+        workoutAdapter.setContext(context!!)
+
         return inflater.inflate(R.layout.fragment_set, container, false).apply {
-            rv_set_exercise.adapter = exerciseGroupAdapter
-            rv_set_exercise.layoutManager = LinearLayoutManager(context)
+            rv_start_sets.adapter = workoutAdapter
+            rv_start_sets.layoutManager = LinearLayoutManager(context)
+        }.also {
+            presenter.fragmentViewCreated(workout)
         }
+    }
+
+    override fun showWorkout(workout: Workout) {
+        et_start_starttime_count.setText(workout.warmUp.toString())
+        et_start_endtime_count.setText(workout.coolDown.toString())
+        workoutAdapter.setWorkout(workout)
+        workoutAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.fragmentViewDestroyed()
+    }
+
+    override fun showExerciseGroup(exerciseGroup: ExerciseGroup) {
+        // TODO("next screen")
     }
 }
--- app/src/main/java/com/atybaty/timer/view/adapters/ExerciseGroupAdapter.kt
+++ app/src/main/java/com/atybaty/timer/view/adapters/ExerciseGroupAdapter.kt
@@ -1,26 +1,48 @@
 package com.atybaty.timer.view.adapters;
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.atybaty.timer.R
+import com.atybaty.timer.WorkoutRepositoryHolder
 import com.atybaty.timer.contract.WorkoutContract
 import com.atybaty.timer.model.ExerciseGroup
+import com.atybaty.timer.model.Workout
 import com.atybaty.timer.view.holders.ExerciseGroupHolder
+import com.atybaty.timer.view.holders.WorkoutHolder
 
-class ExerciseGroupAdapter(
-    private val context: Context,
-    private val presenter: WorkoutContract.Presenter
+class ExerciseGroupAdapter(private val presenter: WorkoutContract.Presenter
 ) : RecyclerView.Adapter<ExerciseGroupHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseGroupHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    private lateinit var workout: Workout
+    private lateinit var context: Context
+
+    fun setContext(context: Context) {
+        this.context = context
     }
 
+    fun setWorkout(workout: Workout) {
+        this.workout = workout
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseGroupHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.item_train_set, parent, false)
+        val holder = ExerciseGroupHolder(itemView)
+        return holder
+    }
+
+
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return workout.exerciseGroups.size
     }
 
     override fun onBindViewHolder(holder: ExerciseGroupHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // TODO("proper name")
+        // val exerciseGroup = workout.exerciseGroups[position]
+        holder.setName("Сет" + position)
     }
+
+
 
 }
